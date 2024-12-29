947ef24f93c3c5e842b526c5339a81b1de1be862
Syed <syedali3334@gmail.cojm>
2024-12-29 23:48:18 +0530

    question2

---
+++ Maths/Question2.java
@@ -0,0 +1,11 @@
+package Maths;
+
+public class Question2 {
+    public static void main(String[] args) {
+          System.out.println(solve(18882,733));
+    }
+    public static int solve(int A, int B )
+    {
+      return Math.abs(A-B);
+    }
+}
